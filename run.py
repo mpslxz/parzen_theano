54a61b1e349a6c26cfeecefbd4a068cae806a8e8
mpslxz <seyedmehran.pesteie@gmail.com>
2018-02-01 12:29:26 -0800

    ENH: use viz engine, try two dists

--- run.py
+++ run.py
@@ -1,39 +1,34 @@
-import matplotlib.pyplot as plt
 from utils.windows import hypercube_window_func as HCW
 from utils.kernels import hypercube as HCK
 from core.parzen import ParzenWindow
+from utils.viz import ParzenViz
+
 
 if __name__ == "__main__":
     import numpy as np
-    P = ParzenWindow(kernel_fcn=HCK, window_fcn=HCW, window_size=1.)
-    x_train = np.random.multivariate_normal(np.zeros(2),
-                                            np.eye(2),
-                                            1000).astype('float32')
-    print x_train.shape
-    x_sample = np.array([[0, 0]]).astype('float32')
+    P = ParzenWindow(kernel_fcn=HCK, window_fcn=HCW, window_size=2.)
+    x_train_normal = np.random.multivariate_normal(np.zeros(2),
+                                                   np.eye(2),
+                                                   1000).astype('float32')
+    x_sample = np.array([[0.5, 0]]).astype('float32')
 
     print 30 * "-"
+    print "Exp. 1"
     print "x_train \t--> 1000 2D points from multivariate N([0, 0], [[1,0],[0,1]])"
-    print "x \t\t--> ", x_sample.squeeze()
-    print "p(x) \t\t--> ", P.estimate_probability(x_train, x_sample)
+    print "x \t\t--> ", x_sample
+    print "p(x) \t\t--> ", P.estimate_probability(x_sample, x_train_normal)
     print 30 * "-"
 
-    size = 5
-    frac = 0.2
-    p = np.arange(-size, size, frac)
-    map_size = (2 * int(size / frac), 2 * int(size / frac))
-    dist = np.zeros(map_size)
-    for i in range(map_size[0]):
-        for j in range(map_size[1]):
-            dist[i, j] = P.estimate_probability(x_train,
-                                                np.array([[p[i], p[j]]]).astype('float32'))
-    plt.subplot(1, 2, 1)
-    plt.scatter(x_train[:, 0], x_train[:, 1], s=0.1)
-    plt.xlim([-4, 4])
-    plt.ylim([-4, 4])
-    plt.title('points')
-    plt.subplot(1, 2, 2)
-    cs = plt.contour(p, p, dist, cmap='jet')
-    plt.clabel(cs, inline=1)
-    plt.title('p(x)')
-    plt.savefig('plot.png')
+    x_train_uniform = np.random.uniform(-3, 3, (1000, 2)).astype('float32')
+    x_sample = np.array([[0.5, 0]]).astype('float32')
+    print "Exp. 2"
+    print "x_train \t--> 1000 2D points from uniform U(-1, 1)"
+    print "x \t\t--> ", x_sample
+    print "p(x) \t\t--> ", P.estimate_probability(x_sample, x_train_uniform)
+    print 30 * "-"
+
+    viz_engine = ParzenViz(
+        dist_list=[x_train_normal, x_train_uniform],
+        dist_names=['normal', 'uniform'],
+        parzen_obj=P)
+    viz_engine.gen_plots()
