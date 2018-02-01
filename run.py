f03b60af2df94da7bc3ef0a73309232250df636d
mpslxz <seyedmehran.pesteie@gmail.com>
2018-01-31 20:28:12 -0800

    FIX: replace with true normal multivariate

--- run.py
+++ run.py
@@ -6,11 +6,14 @@ from core.parzen import ParzenWindow
 if __name__ == "__main__":
     import numpy as np
     P = ParzenWindow(kernel_fcn=HCK, window_fcn=HCW, window_size=1.)
-    x_train = np.random.normal(0, 1., (10000, 2)).astype('float32')
+    x_train = np.random.multivariate_normal(np.zeros(2),
+                                            np.eye(2),
+                                            1000).astype('float32')
+    print x_train.shape
     x_sample = np.array([[0, 0]]).astype('float32')
 
     print 30 * "-"
-    print "x_train \t--> 10000 2D points from N(0, 1)"
+    print "x_train \t--> 1000 2D points from multivariate N([0, 0], [[1,0],[0,1]])"
     print "x \t\t--> ", x_sample.squeeze()
     print "p(x) \t\t--> ", P.estimate_probability(x_train, x_sample)
     print 30 * "-"
@@ -25,7 +28,7 @@ if __name__ == "__main__":
             dist[i, j] = P.estimate_probability(x_train,
                                                 np.array([[p[i], p[j]]]).astype('float32'))
     plt.subplot(1, 2, 1)
-    plt.scatter(x_train[:, 0], x_train[:, 1], s=0.01)
+    plt.scatter(x_train[:, 0], x_train[:, 1], s=0.1)
     plt.xlim([-4, 4])
     plt.ylim([-4, 4])
     plt.title('points')
